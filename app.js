62c69884b953963d4a6eca31b9e3eca1898f795f
Brendan Burns <boardslide03@Brendans-MacBook-Pro-2.local>
2016-11-22 19:23:08 -0800

    in class edits

--- app.js
+++ app.js
@@ -1,25 +1,66 @@
 $(document).ready(function() {
 
-    $("body").fadeIn(3000);
+    $("body").fadeIn();
 
-    $("p").mouseenter(function() {
+    $("p").on("mouseenter", function() {
         $(this).css("background-color", "yellow");
 
     });
 
-    $("p").mouseleave(function() {
-        $(this).css("background-color", "none");
-
+    $("p").on("mouseleave", function() {
+        // $(this).css("background-color", "white");
+        $(this).removeAttr("style");
     });
 
     $("h2").on("click", function() {
-        $(this).css({
-            "opacity": "-=25%",
+        $(this).animate({
+            "opacity": ".25",
             "margin-left": "20px"
-        }, function() {
-            $(".speech").fadeOut("opacity", "-50%");
+        }, 2000, function() {
+            $(".speech").animate({
+                "opacity": ".5"
+            });
         });
 
+    });
+
+    $("#switcher-default").on("click", function() {
+        $("body").css("font-size", "62.5%");
+
+    });
+
+    $("#switcher-large").on("click", function() {
+        // $("body").css("font-size", "100%");
+        $("body").css("font-size", "+=5%");
+    });
+
+    $("#switcher-small").on("click", function() {
+        // $("body").css("font-size", "50%");
+        $("body").css("font-size", "-=5%");
+    });
+
+    $(document).on("keydown", function(event) {
+        console.log(event.which, "KEY PRESSED");
+
+        var keyPressed = event.which;
+
+        if (keyPressed === 37) {
+            $("#switcher").stop().animate({
+                "margin-left": "-=20px"
+            });
+        } else if (keyPressed === 38) {
+            $("#switcher").stop().animate({
+                "margin-top": "-=20px"
+            });
+        } else if (keyPressed === 39) {
+            $("#switcher").stop().animate({
+                "margin-left": "+=20px"
+            });
+        } else if (keyPressed === 40) {
+            $("#switcher").stop().animate({
+                "margin-top": "+=20px"
+            });
+        };
     });
 
 });
